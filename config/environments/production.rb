1c3b85a05235e423b46a46c36a6c09b0ba6b3411
Aurora <aurawra@outlook.com>
2025-10-15 19:32:59 -0500

    Disable cable in config
    Disabled cable in environment ruby config files to prevent required setup of cable database or redis solidequeue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,12 +46,16 @@ Rails.application.configure do
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
+  # Disable Cable
+  config.action_cable.mount_path = nil
+  config.action_cable.disable_request_forgery_protection = true
+
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :solid_cache_store
+  # config.cache_store = :solid_cache_store
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
-  config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  # config.active_job.queue_adapter = :solid_queue
+  # config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
