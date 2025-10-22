e7bc8ca5007512274cd071009fdc35d07d73472c
Aurora <aurawra@outlook.com>
2025-10-22 18:24:52 -0500

    Restrict dashboard view
    Restricted dashboard view to only logged in users using current_user session variable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,16 @@
 class PagesController < ApplicationController
+    before_action :require_login, only: [:dashboard]
+
     def home
     end
 
     def dashboard
     end
+
+    # Redirect users to the login page if they are not signed in
+  def require_login
+    unless current_user
+      redirect_to home_path, alert: "You must log in with spotify to access the dashboard."
+    end
+  end
 end
