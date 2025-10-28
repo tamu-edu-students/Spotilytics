060ae51307809bf3fb5ea8fe30ebe251805a224b
Aurora <aurawra@outlook.com>
2025-10-28 15:01:19 -0500

    Re-add prev home page specs
    Changed previous home page request specs to be in the correct request folder and left dashboard preview tests in pages controller spec

---
+++ spec/requests/dashboard_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe "Pages Controller", type: :request do
+    describe "Dashboard" do
+        it "correctly redirects when the user is not logged in" do
+            get dashboard_path
+            expect(response).to redirect_to(home_path)
+        end
+        it "correctly has a popup on home page when the user is not logged in" do
+            get dashboard_path  
+            follow_redirect!
+            expect(response.body).to include("You must log in with spotify to access the dashboard.")
+        end
+    end
+end
