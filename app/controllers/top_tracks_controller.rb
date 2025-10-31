b989ce9c5e6959a43488360d0eb51e6445ce8432
Spoorthy K R <spoorthykr1801@gmail.com>
2025-10-31 15:17:59 -0500

    Added code changes to add a dropdown for selecting how many top tracks to view in the Top Tracks tab

--- app/controllers/top_tracks_controller.rb
+++ app/controllers/top_tracks_controller.rb
@@ -5,11 +5,14 @@ class TopTracksController < ApplicationController
   def index
     client = SpotifyClient.new(session: session)
 
+    limit = params[:limit].to_i
+    limit = 10 unless [10, 25, 50].include?(limit)
+
     begin
       # For sprint-1, we want the data for the last 1 year
       # For next sprint, we can add more data and different ranges.
       # Fetch top 10 long-term tracks (~past 1 year per Spotify API docs)
-      @tracks = client.top_tracks(limit: 10, time_range: "long_term")
+      @tracks = client.top_tracks(limit: limit, time_range: "long_term")
       @error  = nil
     rescue SpotifyClient::UnauthorizedError => e
       Rails.logger.error "Spotify unauthorized: #{e.message}"
