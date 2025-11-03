0d8644be9bbb9c00834c262187ee80c3e624e601
Aurora <aurawra@outlook.com>
2025-11-03 09:22:57 -0600

    Added randomness to reccomendations
    Pulled 20 top artists and 20 top tracks then randomly sampled a combination of 5 from the 2 to add some element of randomness on reccomendations

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -4,11 +4,14 @@ class RecommendationsController < ApplicationController
     def recommendations
         spotify_client = SpotifyClient.new(session: session)
 
-        top_artists = spotify_client.top_artists(limit: 3, time_range: 'medium_term')
-        top_tracks  = spotify_client.top_tracks(limit: 3, time_range: 'medium_term')
+        top_artists = spotify_client.top_artists(limit: 20, time_range: 'medium_term')
+        top_tracks  = spotify_client.top_tracks(limit: 20, time_range: 'medium_term')
 
-        # Combine artist names and track names into search queries
-        search_queries = (top_artists.map(&:name) + top_tracks.map(&:name)).join(' OR ')
+        # Combine and randomly select 5 total seeds
+        sampled_seeds = (top_artists.map(&:name) + top_tracks.map(&:name)).sample(5)
+
+        # Build query for Spotify Search
+        search_queries = sampled_seeds.join(' OR ')
 
         @recommendations = spotify_client.search_tracks(search_queries, limit: 15)
     rescue SpotifyClient::Error => e
