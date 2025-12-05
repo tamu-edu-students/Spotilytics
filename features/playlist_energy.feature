ff5c86ba5c496b17fd3f5676a9c2147ae752c0ed
Charlie Chiu <charly729.chiu@gmail.com>
2025-12-05 16:56:42 -0600

    Exercise playlist energy normalization in Cucumber
    Add scenario stubbing playlist tracks and ReccoBeats features to drive the energy graph through normalize_energy/fetch_feature_map/energy_profile; assert normalized values render.

--- features/playlist_energy.feature
+++ features/playlist_energy.feature
@@ -16,3 +16,12 @@ Feature: Playlist energy rollercoaster
     And I should see "Track Beta"
     And I should see "Energy by track"
     And I should see "75.0%"
+
+  Scenario: Viewing playlist energy with normalized values
+    Given Spotify playlist tracks and features are available
+    When I visit "/playlists/pl-energy/energy"
+    Then I should see "Energy Rollercoaster"
+    And I should see "Playlist ID: pl-energy"
+    And I should see "50.0%"
+    And I should see "100%"
+    And I should see "N/A"
--- features/step_definitions/playlist_energy_steps.rb
+++ features/step_definitions/playlist_energy_steps.rb
@@ -9,3 +9,25 @@ Given("the playlist energy service returns sample points") do
     { label: "2. Track Beta", energy: 42.5, track: OpenStruct.new(name: "Track Beta", artists: "B") }
   ])
 end
+
+Given("Spotify playlist tracks and features are available") do
+  tracks = [
+    OpenStruct.new(id: "t1", name: "One", artists: "A"),
+    OpenStruct.new(id: "t2", name: "Two", artists: "B"),
+    OpenStruct.new(id: "t3", name: "Three", artists: "C")
+  ]
+
+  allow_any_instance_of(SpotifyClient)
+    .to receive(:playlist_tracks)
+    .with(playlist_id: "pl-energy", limit: 100)
+    .and_return(tracks)
+
+  allow(ReccoBeatsClient)
+    .to receive(:fetch_audio_features)
+    .with([ "t1", "t2", "t3" ])
+    .and_return([
+      { "id" => "t1", "energy" => 0.5 },   # 50.0%
+      { "id" => "t2", "energy" => 150 },   # capped to 100%
+      { "id" => "t3", "energy" => nil }    # N/A
+    ])
+end
